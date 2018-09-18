5d256b23ea828a77cc299141977ba781aa4b4501
veit <veit@tpxip.com>
2018-09-18 22:24:20 +0800

    添加代码 	modified:   how2j.cn/JDBC/src/cn/how2j/jdbc/TestJDBC.java

--- how2j.cn/JDBC/src/cn/how2j/jdbc/TestJDBC.java
+++ how2j.cn/JDBC/src/cn/how2j/jdbc/TestJDBC.java
@@ -2,6 +2,8 @@ package cn.how2j.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -10,6 +12,7 @@ public class TestJDBC {
 	public static void main(String[] args) {
 		Connection c = null;
 		Statement s = null;
+		PreparedStatement ps = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			System.out.println("数据库驱动加载成功！");
@@ -35,9 +38,8 @@ public class TestJDBC {
 			// 准备sql语句
             // 注意： 字符串要用单引号'
 			String sql = "insert into hero values(null,'提莫',313,50)";
-			if (s.execute(sql)) {
-				System.out.println("执行插入语句成功");
-			}
+			s.execute(sql);
+			System.out.println("执行插入语句成功");
 			
 			sql = "delete from hero where id = 5";
 			s.execute(sql);
@@ -46,6 +48,47 @@ public class TestJDBC {
 			sql = "update hero set name='name 5' where id = 3";
 			s.execute(sql);
 			System.out.println("执行更新语句成功");
+			
+			s.executeUpdate("delete from hero where name like '英雄%'");
+			
+			for (int i=0; i<20; i++) {
+				sql = "insert into hero values(null,'英雄"+(i+1)+"',"+(313+1)+","+(50+i)+")";
+				s.execute(sql);
+			}
+			System.out.println("插入成功");
+			
+			StringBuilder sb = new StringBuilder();
+			ResultSet rs = s.executeQuery("select * from hero where name like 'hero%'");
+			while (rs.next()) {
+				int id = rs.getInt("id");
+				String name = rs.getString(2);
+				float hp = rs.getFloat("hp");
+				int damage = rs.getInt(4);
+				sb.append(name).append(',');
+			}
+			System.out.println(sb.toString());
+			
+			if (s.execute("select count(*) from hero")) {//返回值为true，表示执行的是查询语句
+				rs = s.getResultSet();
+				if (rs.next()) {
+					System.out.println("[execute] 总记录数 "+rs.getInt(1));
+				}
+			}
+			
+			rs = s.executeQuery("select count(*) from hero");
+			if (rs.next()) {
+				System.out.println("[executeQuery] 总记录数 "+rs.getInt(1));
+			}
+			
+			ps = c.prepareStatement("insert into hero values(null,?,?,?)", Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, "呵呵");
+			ps.setFloat(2, 666.6f);
+			ps.setInt(3, 66);
+			ps.execute();
+			rs = ps.getGeneratedKeys();
+			if (rs.next()) {
+				System.out.println("[getGeneratedKeys] 插入后自动产生的id= "+rs.getInt(1));
+			}
 			
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
@@ -61,6 +104,9 @@ public class TestJDBC {
                     e.printStackTrace();
                 }
             }
+            
+            if (ps != null) { try { ps.close(); } catch (Exception e) { e.printStackTrace(); }}
+            
             // 后关闭Connection
             if (c != null) {
                 try {
